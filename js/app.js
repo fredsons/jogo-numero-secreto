4c218ab688df2f996880e3583e11207f8c841cc3
Fredson <fredsondev@gmail.com>
2023-05-18 18:30:45 -0300

    atualizacao

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-// const menorValor = 1;
-// const maiorValor = 1000;
-// const numeroSecreto = gerarNumeroAleatorio();
-
-// function gerarNumeroAleatorio() {
-//   return Math.floor(Math.random() * (maiorValor - menorValor + 1));
-// }
-
-// console.log('Número Secreto:', numeroSecreto);
-
-// const elementoMenorValor = document.getElementById('menor-valor');
-// elementoMenorValor.innerHTML = menorValor;
-
-// const elementoMaiorValor = document.getElementById('maior-valor');
-// elementoMaiorValor.innerHTML = maiorValor;
-
 
 const menorValor = 1;
 const maiorValor = 1000;
